b113e386974ce6b959f881eb8753ad132ce42215
pengmengnanlan <llanan9817@163.com>
2019-02-15 17:44:40 +0200

    alina first commit

--- SurveyPWP/app.py
+++ SurveyPWP/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+
+
 app = Flask("SurveyPWP")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
